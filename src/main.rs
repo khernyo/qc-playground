2674339228ae96c0423b5f61a4c2c5383fccada6
Szabolcs Berecz <szabolcs.berecz@gmail.com>
2018-01-11 10:27:21 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ extern crate num_complex;
 extern crate num_traits;
 extern crate rand;
 
+use std::fmt::{self, Display, Formatter};
+
 use nalgebra::*;
 use num_complex::{Complex, Complex64};
 use num_traits::identities::One;
@@ -12,7 +14,7 @@ use num_traits::identities::Zero;
 use rand::Rng;
 
 fn main() {
-    println!("Hello, world! {:?}", QBit::one());
+    println!("{}", QGate1::hadamard().apply(&QBit::zero()));
 }
 
 // TODO This should be a Unit<Vector2<Complex<f64>>>
@@ -29,20 +31,16 @@ impl QBit {
 
     /// |0>
     fn zero() -> QBit {
-        let q = QBit {
-            a: Complex64::new(1f64, 0f64),
-            b: Complex64::new(0f64, 0f64),
-        };
-        q.validate();
-        q
+        QBit::from_components(Complex64::new(1f64, 0f64), Complex64::new(0f64, 0f64))
     }
 
     /// |1>
     fn one() -> QBit {
-        let q = QBit {
-            a: Complex64::new(0f64, 0f64),
-            b: Complex64::new(1f64, 0f64),
-        };
+        QBit::from_components(Complex64::new(0f64, 0f64), Complex64::new(1f64, 0f64))
+    }
+
+    fn from_components(a: Complex64, b: Complex64) -> QBit {
+        let q = QBit { a, b };
         q.validate();
         q
     }
@@ -69,33 +67,65 @@ impl QBit {
     }
 }
 
-struct CNot(Matrix4<Complex64>);
+fn format_complex(c: &Complex64) -> String {
+    return if relative_eq!(c.im, 0f64) {
+        format!("{}", c.re)
+    } else {
+        format!("({}+{}i)", c.re, c.im)
+    };
+}
 
-impl CNot {
-    fn new() -> CNot {
-        CNot(Matrix4::new(
-            1f64.into(),
-            0f64.into(),
-            0f64.into(),
-            0f64.into(),
-            0f64.into(),
-            1f64.into(),
-            0f64.into(),
-            0f64.into(),
-            0f64.into(),
-            0f64.into(),
-            0f64.into(),
-            1f64.into(),
-            0f64.into(),
-            0f64.into(),
-            1f64.into(),
-            0f64.into(),
-        ))
+impl Display for QBit {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), fmt::Error> {
+        let (zp, op) = self.prob();
+        if relative_eq!(zp, 1f64) {
+            write!(f, "|0>");
+        } else if relative_eq!(op, 1f64) {
+            write!(f, "|1>");
+        } else {
+            let a = format_complex(&self.a);
+            let b = format_complex(&self.b);
+            write!(f, "{}|0> + {}|1>", a, b);
+        }
+        Ok(())
+    }
+}
+
+struct QGate1(Matrix2<Complex64>);
+
+impl QGate1 {
+    fn hadamard() -> QGate1 {
+        let m: Matrix2<Complex64> = Matrix2::from_rows(&[
+            RowVector2::new(1f64.into(),   1f64.into()),
+            RowVector2::new(1f64.into(), (-1f64).into()),
+        ]);
+        QGate1(m / Complex64::from(2f64.sqrt()))
+    }
+
+    fn apply(&self, q: &QBit) -> QBit {
+        let v = self.0 * Vector2::new(q.a, q.b);
+        let qr = QBit::from_components(v[0], v[1]);
+        qr
+    }
+}
+
+struct QGate2(Matrix4<Complex64>);
+
+impl QGate2 {
+    fn cnot() -> QGate2 {
+        QGate2(Matrix4::from_rows(&[
+            RowVector4::new(1f64.into(), 0f64.into(), 0f64.into(), 0f64.into()),
+            RowVector4::new(0f64.into(), 1f64.into(), 0f64.into(), 0f64.into()),
+            RowVector4::new(0f64.into(), 0f64.into(), 0f64.into(), 1f64.into()),
+            RowVector4::new(0f64.into(), 0f64.into(), 1f64.into(), 0f64.into()),
+        ]))
     }
 
     fn apply(&self, q1: &QBit, q2: &QBit) -> (QBit, QBit) {
         let v = self.0 * Vector4::new(q1.a, q1.b, q2.a, q2.b);
-        (QBit { a: v[0], b: v[1] }, QBit { a: v[2], b: v[3] })
+        let qr1 = QBit::from_components(v[0], v[1]);
+        let qr2 = QBit::from_components(v[2], v[3]);
+        (qr1, qr2)
     }
 }
 
@@ -110,5 +140,15 @@ mod test {
 
         let qq = q.measure(&mut rng);
         assert!(false, "{:?}", qq);
+    }
+
+    #[test]
+    fn test_cnot() {
+        let mut rng = rand::thread_rng();
+        let q0 = QBit::zero();
+        let q1 = QBit::one();
+
+        let (qq0, qq1) = QGate2::cnot().apply(&q0, &q1);
+        assert!(false, "CNot({:?}, {:?}) = ({:?}, {:?})", q0, q1, qq0, qq1);
     }
 }
